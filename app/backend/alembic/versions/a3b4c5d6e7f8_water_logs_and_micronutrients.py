6eedabdf575cc7a63097fa06f3c319329f6202a7
PavelKobe <kobelev.paw@yandex.ru>
2026-03-28 14:53:31 +0300

    объединение миграций

--- app/backend/alembic/versions/a3b4c5d6e7f8_water_logs_and_micronutrients.py
+++ app/backend/alembic/versions/a3b4c5d6e7f8_water_logs_and_micronutrients.py
@@ -13,7 +13,7 @@ from alembic import op
 revision: str = "a3b4c5d6e7f8"
 down_revision: Union[str, Sequence[str], None] = "f1a2b3c4d5e6"
 branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = ["f2a3b4c5d6e7"]
 
 
 def upgrade() -> None:

8b35c490b653c1202b1ecbe5533fb03ddbe6bdec
PavelKobe <kobelev.paw@yandex.ru>
2026-03-28 15:07:12 +0300

    Fix alembic migration dependency

--- app/backend/alembic/versions/a3b4c5d6e7f8_water_logs_and_micronutrients.py
+++ app/backend/alembic/versions/a3b4c5d6e7f8_water_logs_and_micronutrients.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "a3b4c5d6e7f8"
-down_revision: Union[str, Sequence[str], None] = "f1a2b3c4d5e6"
+down_revision: Union[str, Sequence[str], None] = "f2a3b4c5d6e7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = ["f2a3b4c5d6e7"]
 
